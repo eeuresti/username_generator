cb94202229e725c03ae71a06854b2f9eb8fec331
Eric Euresti <eeuresti@gmail.com>
2015-09-15 16:22:54 -0700

    format_name in the works

--- username.rb
+++ username.rb
@@ -2,8 +2,15 @@
 # Run `rspec /spec/username_spec.rb` to get started.
 
 def format_name(first, last)
-  nil
+  if first == "" || last == ""
+    return nil
+  end
+  new_first = first.gsub(/\W+/,"")
+  new_last = last.gsub(/\W+/,"")
+  username = new_first[0] + new_last
+  username = username.downcase
 end
+
 
 def format_year
   nil
