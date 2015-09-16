7f0eee31ab97945e4b7c5205296b55379da92d5f
Eric Euresti <eeuresti@gmail.com>
2015-09-15 17:15:17 -0700

    generate_username still not working

--- username.rb
+++ username.rb
@@ -28,3 +28,28 @@ def build_username(first, last, year, user_type=0)
   username = username + format_year(year)
   return username = type_array[user_type] + username
 end
+
+
+
+  usernames = {}
+def generate_username(first_name, last_name, birth_year, privilege_level=0)
+  new_username = build_username(first_name, last_name, birth_year, privilege_level)
+
+  if usernames[new_username] == nil
+    usernames[new_username] = 0
+    return new_username
+  else
+    usernames[new_username] += 1
+    new_username = new_username + "_" + usernames[new_username].to_s
+    return new_username
+  end
+  # usernames.index(new_username)
+  # if usernames.index(new_username) == nil
+  #   usernames.push(new_username)
+  #   return new_username
+  # else
+  #   new_username = new_username + "_1"
+  #   usernames.push(new_username)
+  #   return new_username
+  # end
+end
